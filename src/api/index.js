ba55b68ee7e004fd52159d5bfbc764959d285cb8
StasGil <stanislav.gill@jadexconsulting.com>
2023-08-29 14:02:26 +0200

    updated api

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //const url = "http://localhost:5000/";
 
-const url = "https://notice-a.herokuapp.com/";
+const url = "https://node-notice.onrender.com";
 
 const API = axios.create({ baseURL: url });
 
